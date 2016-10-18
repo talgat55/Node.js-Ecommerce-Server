a9c29f8a5c029e9f49fe9bd0592708dc704fdf6b
talgat55 <lightx_design@yahoo.com>
2016-10-18 22:40:31 +0600

    first commit add postgress config

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node.js-Ecommerce-Server
---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+import db from "./db/db.js"
+
+//var debug = require('debug')('my-application');
+//var app = require('../app');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+// environment
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('env', process.env.mode = 'development');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+} else {
+
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
+
+
+
+
+var server = app.listen(app.get('port'), function() {
+    //debug('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
+});
